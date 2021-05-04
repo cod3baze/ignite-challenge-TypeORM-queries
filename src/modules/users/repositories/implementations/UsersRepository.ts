6dcbbb633f2400d2f06ec082058f5b70b4010105
eliasall3x <elliasallex@gmail.com>
2021-05-03 23:59:42 -0300

    feat: test case-sensitive resolved (findUserByFullName)

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -34,7 +34,10 @@ export class UsersRepository implements IUsersRepository {
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
     return this.repository.query(
-      `SELECT * FROM users WHERE first_name = LOWER(${first_name}) AND last_name= LOWER(${last_name})`
+      `SELECT * FROM users
+        WHERE LOWER(first_name) = LOWER('${first_name}')
+        AND
+        LOWER(last_name) = LOWER('${last_name}')`
     );
   }
 }
